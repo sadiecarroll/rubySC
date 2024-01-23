df5d82780f5ab2f5ee258749e386a17e8995e595
Sadie Carroll <sadie.carroll@kellogg.northwestern.edu>
2024-01-23 15:32:32 +0000

    lab 2 complete

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -12,4 +12,36 @@
 
 # Elsif Conditional Logic
 
-# Combining Expressions
+# Combining 
+
+# #input
+# favorite_food = "tacos"
+# lunch = "tacos"
+
+# #output
+# if lunch == favorite_food
+#     puts "yay"
+# end
+
+# # input 
+# real_password = "tacos"
+# user_entered_password = "secret"
+
+# # if user entered password is the same as real password, let me in
+# if real_password == user_entered_password
+#     puts "You're In!"
+# else
+# puts "Ooops, that's the wrong password!"
+# end
+
+
+temperature = 65
+precip = 0
+
+if temperature > 60 && precip == 0 
+    puts "Nice!"
+elsif temperature > 60 && precip > 0
+    puts "sad trombone" 
+else 
+    puts "it's gross"
+end
